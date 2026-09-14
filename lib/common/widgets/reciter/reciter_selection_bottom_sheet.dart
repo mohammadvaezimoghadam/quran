efa8a43d58df61a9906e340c1c89561faeed7d94
Mohammad Vaezi Moghadam <mohammadvaezim78@gmail.com>
2026-09-14 06:43:14 -0700

    feat(reciter): show VIP subscription sheet when tapping locked reciter

--- lib/common/widgets/reciter/reciter_selection_bottom_sheet.dart
+++ lib/common/widgets/reciter/reciter_selection_bottom_sheet.dart
@@ -13,6 +13,7 @@ import '../../../features/quran_reader/domain/entities/reciter_entity.dart';
 import '../../../features/quran_reader/presentation/utils/reciter_download_helper.dart';
 import '../../../features/subscription/domain/policy/audio_vip_policy.dart';
 import '../../../features/subscription/application/vip_subscription_controller.dart';
+import '../../../features/subscription/presentation/ui/vip_subscription_sheet.dart';
 
 /// Model representing a unique Reciter person with all their recitation variants.
 class ReciterGroup {
@@ -620,6 +621,10 @@ class _ReciterSelectionBottomSheetState
                           color: Colors.transparent,
                           child: InkWell(
                             onTap: () async {
+                              if (isLocked) {
+                                VipSubscriptionSheet.show(context);
+                                return;
+                              }
                               if (widget.isDownloadMode) {
                                 widget.onReciterSelected?.call(activeVariant);
                                 Navigator.of(context).pop();
@@ -780,6 +785,15 @@ class _ReciterSelectionBottomSheetState
                                       ),
                                       tooltip: 'تغییر سبک تلاوت',
                                       onSelected: (variant) async {
+                                        final variantIsLocked = !isVip &&
+                                            (widget.isTranslationMode
+                                                ? true
+                                                : !AudioVipPolicy.isDefaultReciter(
+                                                    variant.identifier));
+                                        if (variantIsLocked) {
+                                          VipSubscriptionSheet.show(context);
+                                          return;
+                                        }
                                         setState(() {
                                           _selectedVariantsMap[group.baseName] =
                                               variant;
